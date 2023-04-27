ec81b48625a753368c05943359e3a2800db71e55
TawanAlencar <josetawan300@gmail.com>
2023-04-27 17:12:39 -0300

    Render Cards Profile

--- frontend/src/contexts/profileContexts.tsx
+++ frontend/src/contexts/profileContexts.tsx
@@ -1,11 +1,12 @@
 import { ReactNode, createContext, useContext, useState } from "react";
-import { IAnuncio, IUser } from "../types/home/homeInterface";
+import { IAnuncio } from "../types/home/homeInterface";
 import { api } from "../services/api";
+
+
 export interface IProfileContext {
   announcementsAdmin: IAnuncio[];
   setAnnouncementsAdmin: React.Dispatch<React.SetStateAction<IAnuncio[]>>;
- listAnnouncementsAdmin: (id: string) => Promise<void>;
- 
+  listAnnouncementsAdmin: (id: string) => Promise<void>;
 }
 
 interface IProfile {
@@ -18,30 +19,26 @@ export const ProfileContext = createContext<IProfileContext>(
 export const ProfileProvider = ({ children }: IProfile) => {
   const [announcementsAdmin, setAnnouncementsAdmin] = useState<IAnuncio[]>([]);
 
-  
-
   const token = localStorage.getItem("token");
 
-
-    const listAnnouncementsAdmin = async (id: string): Promise<void> => {
-        try {
-        const { data } = await api.get(`/announcement/user/${id}`, {
-            headers: { authorization: `Bearer ${token}` },
-        });
-        setAnnouncementsAdmin(data);
-        return data;
-        } catch (error) {
-        console.error(error);
-        }
-  }
+  const listAnnouncementsAdmin = async (id: string): Promise<void> => {
+    try {
+      const { data } = await api.get(`/announcement/user/${id}`, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      setAnnouncementsAdmin(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <ProfileContext.Provider
       value={{
         announcementsAdmin,
         setAnnouncementsAdmin,
-        listAnnouncementsAdmin
-        
+        listAnnouncementsAdmin,
       }}
     >
       {children}
