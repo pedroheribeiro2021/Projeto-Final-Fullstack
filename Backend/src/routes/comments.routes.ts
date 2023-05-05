577a4cf9dfe440b87e47c4669144811f4fbd0986
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-05-05 14:26:19 -0300

    fix comment route

--- Backend/src/routes/comments.routes.ts
+++ Backend/src/routes/comments.routes.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
 import { createCommentsController, listCommentsController } from "../controllers/comment/comment.controller";
-import { ensureAuthMiddleware } from "../middlewares/user.middlewares/ensureAuth.middleware";
-
 
 export const commentsRoutes = Router();
 
 commentsRoutes.get("/:id", listCommentsController)
-commentsRoutes.post("/:id", ensureAuthMiddleware, createCommentsController)
+commentsRoutes.post("/:id", createCommentsController)
