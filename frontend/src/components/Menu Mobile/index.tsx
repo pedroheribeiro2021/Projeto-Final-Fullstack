29691c9543486c55cae13e967d2ab1bd89ae68a1
TawanAlencar <josetawan300@gmail.com>
2023-05-10 19:21:02 -0300

    Fix: bug header mobile

--- frontend/src/components/Menu Mobile/index.tsx
+++ frontend/src/components/Menu Mobile/index.tsx
@@ -5,11 +5,14 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { api } from "../../services/api";
 import { IUser } from "../../types/home/homeInterface";
+import { useLogin } from "../../contexts/loginContext";
 
 export const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [user, setUser] = useState<IUser>();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { setIsEditUserModalOpen, setIsEditAddressModalOpen } =
+  useLogin();
 
   const getUser = async (userId: any) => {
     try {
@@ -19,6 +22,13 @@ export const Menu = () => {
       console.error(error)
     }
 
+  };
+  const handleEditUser = () => {
+    setIsEditUserModalOpen(true);
+  };
+
+  const handleEditAddress = () => {
+    setIsEditAddressModalOpen(true);
   };
 
   useEffect(() => {
@@ -54,15 +64,15 @@ export const Menu = () => {
                 <img src={user1} alt={user.name} />
                 <span>{user.name}</span>
               </div>
-              <Link to="/" className="button_items" onClick={handleMenuClick}>
-                Home
+              <Link to="/profile/admin" className="button_items" onClick={handleMenuClick}>
+                Meu Perfil
               </Link>
-              <Link to="/profile" className="button_items" onClick={handleMenuClick}>
-                Perfil
-              </Link>
+              <button onClick={handleEditUser} className="button_items">Editar usuário</button>
+              <button onClick={handleEditAddress} className="button_items">Editar endereço</button>
               <button onClick={handleSignOut} className="button_items">
                 Sair
               </button>
+              
             </div>
           ) : (
             <div className="menu_items">
